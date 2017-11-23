074ff8b9f87c6ba7e5db3ab9d75b07900620ba19
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2017-11-22 23:08:20 -0800

    SSH command output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "ip_address" {
+output "public_ip" {
   value = "${module.instance.public_ip}"
+}
+
+output "ssh_command" {
+  value = "ssh -i ./keys/fastai-dev-key ubuntu@${module.instance.public_ip}"
 }
