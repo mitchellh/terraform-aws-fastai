f851010224cbb43b4aff6c9d74980a5cda0a9a51
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2017-11-27 09:11:00 -0800

    Update outputs to add descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "public_ip" {
+  description = "Public IP of the Fast.ai course instance."
   value = "${module.instance.public_ip}"
 }
 
 output "ssh_command" {
+  description = "Command to use to immediately SSH into the instance. This is pre-configured with the temporary SSH key created."
   value = "ssh -i ${local.private_key_filename} ubuntu@${module.instance.public_ip}"
 }
